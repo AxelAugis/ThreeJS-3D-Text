774d01e06356d2c7e8bce77f9cfc65753bb88f74
Axel <augisax@gmail.com>
2024-02-14 19:20:03 +0100

    Tried github connection to vercel a second time

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Jade Winum',
+            'Dis pas merci en fait',
             {
                 font: font,
                 size: 0.5,
