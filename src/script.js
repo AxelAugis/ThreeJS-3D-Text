834191ae3d2b269183fa7e19167a5dc5901bfc4d
Axel <augisax@gmail.com>
2024-02-14 19:16:54 +0100

    Tried github connection to vercel

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Bruno Simon',
+            'Jade Winum',
             {
                 font: font,
                 size: 0.5,
